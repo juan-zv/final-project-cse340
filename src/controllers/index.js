37c154fa837338632c5822c2b5a83546fb416e73
juan-zv <juanzuritavasquez@gmail.com>
2026-03-25 19:18:29 -0600

    Add new controller files for admin, catalog, employee, inventory, reviews, service requests, services, and user

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,10 +7,6 @@ const aboutPage = (req, res) => {
     res.render('about', { title: 'About' });
 };
 
-const demoPage = (req, res) => {
-    res.render('demo', { title: 'Middleware Demo Page' });
-};
-
 const testErrorPage = (req, res, next) => {
     const err = new Error('This is a test error');
     err.status = 500;
@@ -18,3 +14,13 @@ const testErrorPage = (req, res, next) => {
 };
 
 export { homePage, aboutPage, demoPage, testErrorPage };
+
+// Export all domain controllers from here to act as a central hub
+export * as adminController from './admin/index.js';
+export * as catalogController from './catalog/index.js';
+export * as employeeController from './employee/index.js';
+export * as inventoryController from './inventory/index.js';
+export * as reviewsController from './reviews/index.js';
+export * as serviceRequestsController from './service-requests/index.js';
+export * as servicesController from './services/index.js';
+export * as userController from './user/index.js';
